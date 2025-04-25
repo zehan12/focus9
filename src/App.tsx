2b4ee41b5512cafe4305d4e9084204d82eeb1ba8
zehan12 <zehan9211@gmail.com>
2025-04-26 00:31:38 +0530

    integrate shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
       <h1 className='text-3xl underline text-red-600 text-center font-mono'>Focus 9</h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+
     </>
   )
 }
